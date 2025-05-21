d5ddefc80bbc3e0b2b3c093d680910948606a637
IgorPeli <103079100+IgorPeli@users.noreply.github.com>
2025-05-21 19:56:15 +0000

    x

--- terraform-code/backends.tf
+++ terraform-code/backends.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "remote" {
     organization = "Igor_Terraform" # Troque pelo nome exato da sua organização
+    hostname     = "app.terraform.io"
 
     workspaces {
       name = "terraform-aws" # Troque pelo nome exato do seu workspace
