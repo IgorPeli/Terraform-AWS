e0c43a8dddb7ce434e735cf167ba104862a7067b
IgorPeli <103079100+IgorPeli@users.noreply.github.com>
2025-05-22 20:08:02 +0000

    x

--- terraform-code/bdlayer.tf
+++ terraform-code/bdlayer.tf
@@ -1,7 +1,30 @@
-resource "aws_db_instance" "InstanceDB" {
-    instance_class = "db.t4g.small"
-    engine = "aurora-mysql"
-    db_name = "wordpressDB"
-    
-  
+
+resource "aws_rds_cluster" "rds_cluster" {
+  cluster_identifier = "wordpresscluster"
+  availability_zones = ["us-west-1a", "us-west-1b"]
+  engine             = "aurora-mysql"
+  engine_version     = "8.0.mysql_aurora.3.04.2"
+  master_username    = "wp_user1"
+  master_password    = "wpadmin1"
+  database_name      = "Wordpress"
+
 }
+
+resource "aws_rds_cluster_instance" "instanceA" {
+  engine             = aws_rds_cluster.rds_cluster.engine
+  engine_version     = aws_rds_cluster.rds_cluster.engine_version
+  instance_class     = "db.t4g.small"
+  cluster_identifier = aws_rds_cluster.rds_cluster.id
+  availability_zone  = "us-west-1a"
+
+}
+
+resource "aws_rds_cluster_instance" "instanceB" {
+  engine             = aws_rds_cluster.rds_cluster.engine
+  engine_version     = aws_rds_cluster.rds_cluster.engine_version
+  instance_class     = "db.t4g.small"
+  cluster_identifier = aws_rds_cluster.rds_cluster.id
+  availability_zone  = "us-west-1b"
+
+}
+
--- terraform-code/lblayer.tf
+++ terraform-code/lblayer.tf
@@ -30,8 +30,8 @@ resource "aws_lb" "LoadBalancer" {
 
 resource "aws_lb_listener" "Listener" {
   load_balancer_arn = aws_lb.LoadBalancer.arn
-  port = "80"
-  protocol = "HTTP"
+  port              = "80"
+  protocol          = "HTTP"
 
 
   default_action {
